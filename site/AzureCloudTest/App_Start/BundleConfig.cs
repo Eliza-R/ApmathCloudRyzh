f04e3d5aa9e4c0a21db7fc86c827fa01aa84b242
Eliza-R <liz.ryzhko@gmail.com>
2016-02-03 02:14:04 +0300

    d3.js bundle

--- site/AzureCloudTest/App_Start/BundleConfig.cs
+++ site/AzureCloudTest/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace AzureCloudTest
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/d3").Include(
+                      "~/Scripts/d3/d3.min.js"));
         }
     }
 }
